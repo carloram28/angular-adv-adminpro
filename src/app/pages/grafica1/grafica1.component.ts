90c4d8d0d929606b1267c58573fe2936efbebb1e
Carlo Ramirez <carloramirez1295@gmail.com>
2023-09-10 20:18:11 -0500

    Fin de seccion 3

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { ChartData } from 'chart.js';
+
+
 
 @Component({
   selector: 'app-grafica1',
@@ -7,5 +10,16 @@ import { Component } from '@angular/core';
   ]
 })
 export class Grafica1Component {
+public labels1:string[]=['Pan','Refresco','Tacos']
+public data1: ChartData<'doughnut'> = {
+  labels: this.labels1,
+  datasets: [
+    { data: [10, 15, 40],
+      backgroundColor:['#6857E6','#009FEE','#F02059']
+    },
+
+  ],
+};
 
 }
+
